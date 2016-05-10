7dd8e8ea0dd7aee6192df579d4683c109492e4e3
Marcelo Gondim <mgmarques94@gmail.com>
2016-05-10 14:03:24 -0300

    tirando um bug no speech

--- public/js/text-recognizer.js
+++ public/js/text-recognizer.js
@@ -8,9 +8,9 @@ var audio = $('.audio').get(0);
 
 getSpeech = function(text){
 
-	var URL = 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=pt-BR_IsabelaVoice&text=' + text;
+	var URL = '/api/synthesize?voice=pt-BR_IsabelaVoice&text=' + text;
 
-	console.log('Falando' + text);
+	console.log('Falando -> ' + text);
 	//URL += '&download=true';
 
 	audio.pause();
